0958196e2b0b2d2492a280fcafa5add51f8057b5
janstoltman <stoltmanjan@gmail.com>
2018-06-06 12:14:53 +0200

    Code refresh

--- app/src/main/java/com/example/yggdralisk/qwertyshirts/MyListAdapter.java
+++ app/src/main/java/com/example/yggdralisk/qwertyshirts/MyListAdapter.java
@@ -4,33 +4,37 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import io.reactivex.annotations.NonNull;
 
 /**
  * Created by yggdralisk on 30.05.16.
  */
 public class MyListAdapter extends BaseAdapter {
 
-    private final String[] urls;
+    private final ArrayList<String> imageUrls = new ArrayList<>();
     private final Context context;
 
-    public MyListAdapter(String[] urls,Context context) {
-        this.urls = urls;
+    MyListAdapter(ArrayList<String> imageUrls, Context context) {
+        this.imageUrls.addAll(imageUrls);
         this.context = context;
     }
 
     @Override
     public int getCount() {
-        return urls.length;
+        return imageUrls.size();
     }
 
     @Override
-    public Object getItem(int position) {
-        return urls[position];
+    @NonNull
+    public String getItem(int position) {
+        return imageUrls.size() > position ? imageUrls.get(position) : "";
     }
 
     @Override
@@ -40,20 +44,17 @@ public class MyListAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-       View v = convertView;
-        if(v == null){
+        View v = convertView;
+        if (v == null) {
             LayoutInflater li = LayoutInflater.from(context);
-            v = li.inflate(R.layout.list_element,null);
+            v = li.inflate(R.layout.list_element, null);
         }
 
-        String url = (String)getItem(position);
+        String url = getItem(position);
+        ImageView imv = (ImageView) v.findViewById(R.id.image);
 
-        if (url != null && url != "") {
-            ImageView imv = (ImageView) v.findViewById(R.id.image);
-
-            if (imv != null) {
-                Glide.with(context).load(url).into(imv);
-            }
+        if (imv != null) {
+            Glide.with(context).load(url).into(imv);
         }
 
         return v;
--- app/src/main/java/com/example/yggdralisk/qwertyshirts/SplashScreen.java
+++ app/src/main/java/com/example/yggdralisk/qwertyshirts/SplashScreen.java
@@ -1,26 +1,42 @@
 package com.example.yggdralisk.qwertyshirts;
 
+import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
+import android.support.v7.app.AppCompatActivity;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
 
 public class SplashScreen extends AppCompatActivity {
+    private static final String HTTPS_PREFIX = "https:";
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    @Override
+    protected void onDestroy() {
+        if (!compositeDisposable.isDisposed()) {
+            compositeDisposable.dispose();
+        }
+
+        super.onDestroy();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        Glide.with(this).load(R.drawable.logo).into((ImageView)findViewById(R.id.SplashImage));
+
         try {
             getElements();
         } catch (IOException e) {
@@ -28,44 +44,57 @@ public class SplashScreen extends AppCompatActivity {
         }
     }
 
-    private void getElements() throws IOException {
-        new ElemGetter().execute();
-    }
-
-    private void gotoMain(String[] res) {
-        Intent intent = new Intent(getApplicationContext(), ActivityMain.class);
+    private void proceedToMainActivity(ArrayList<String> res) {
+        Intent intent = new Intent(this, ActivityMain.class);
         intent.putExtra(getString(R.string.urls_extra_string), res);
         startActivity(intent);
         this.finish();
     }
 
-    private class ElemGetter extends AsyncTask<Void, Void, String[]> {
+    private void getElements() throws IOException {
+        DisposableObserver<ArrayList<String>> imagesObserver = new DisposableObserver<ArrayList<String>>() {
+            @Override
+            public void onNext(ArrayList<String> imageUrls) {
+                proceedToMainActivity(imageUrls);
+            }
 
-        @Override
-        protected String[] doInBackground(Void... params) {
-            Elements temp = null;
-            try {
-                Document doc = Jsoup.connect(getString(R.string.qwertee_url)).get();
-                temp = doc.getElementsByClass("dynamic-image-design");
-            } catch (IOException e) {
+            @Override
+            public void onError(Throwable e) {
                 e.printStackTrace();
             }
-            String[] urls = new String[3];
-            for (int i = 0; i < 3; i++) {
-                assert temp != null;
-                urls[i] = temp.get(i).attr("src");
-            }
 
-            return urls;
+            @Override
+            public void onComplete() {
+
+            }
+        };
+
+        compositeDisposable.add(imagesObserver);
+
+        Observable.fromCallable(() -> scrapImages(getApplicationContext()))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(imagesObserver);
+    }
+
+    private ArrayList<String> scrapImages(Context context) throws IOException {
+        Elements dynamicImageWraps = null;
+        ArrayList<String> imageUrls = new ArrayList<String>();
+
+        Document doc = Jsoup.connect(context.getString(R.string.qwertee_url)).get();
+        dynamicImageWraps = doc.getElementsByClass("design-dynamic-image-wrap");//"zoom-dynamic-image design-dynamic-image");
+
+        for (Element elem : dynamicImageWraps) {
+            imageUrls.add(
+                    HTTPS_PREFIX.concat(elem.select("div").first()
+                            .select("picture").first()
+                            .select("img")
+                            .attr("src"))
+
+            );
         }
 
-        @Override
-        protected void onPostExecute(String[] res) {
-            super.onPostExecute(res);
-            for(int i = 0; i <res.length; i++){
-                res[i] = res[i].replace("//cdn","https://www");
-            }
-            gotoMain(res);
-        }
+
+        return imageUrls;
     }
 }
